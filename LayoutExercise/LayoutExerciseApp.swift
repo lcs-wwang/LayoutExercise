d42df7b4a400d7cb558050c7d267196b3d56b76d
winstonwang <wwang@lcs.on.ca>
2021-10-26 14:17:32 -0400

    Initial Commit

---
+++ LayoutExercise/LayoutExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExerciseApp.swift
+//  LayoutExercise
+//
+//  Created by Winston Wang on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
